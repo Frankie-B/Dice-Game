7759b7399e58a74dc04eac6b602033e54614ac54
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-03-08 14:25:52 +0300

    Final Dice game

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Randomly Generated Nunmber between 1 and 6
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImage1 = './images/dice' + randomNumber1 + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', randomImage1);
+
 var randomImage2 = './images/dice' + randomNumber2 + '.png';
 
+document.querySelectorAll('img')[1].setAttribute('src', randomImage2);
+
 if (randomNumber1 === randomNumber2) {
-  var winningPlayer = 'Draw';
+  document.querySelector('h1').innerHTML = 'Draw';
 } else if (randomNumber1 > randomNumber2) {
-  winningPlayer = '🚩 Player 1 Wins!';
+  document.querySelector('h1').innerHTML = '🚩Player 1 Wins!';
 } else if (randomNumber1 < randomNumber2) {
-  winningPlayer = 'Player 2 Wins! 🚩';
+  document.querySelector('h1').innerHTML = 'Player 2 Wins!🚩';
 } else {
-  winningPlayer = 'No winners here!';
+  document.querySelector('h1').innerHTML = 'No winners here!';
 }
